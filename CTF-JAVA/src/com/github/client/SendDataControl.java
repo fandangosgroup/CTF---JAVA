6960a77b32848546a2e401cc5ba61f726cc01b16
fabegalo <fabegalo@gmail.com>
2020-04-13 19:04:23 -0300

    Socket Cliente - Servidor

---
+++ CTF-JAVA/src/com/github/client/SendDataControl.java
@@ -0,0 +1,57 @@
+package com.github.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+  
+public class SendDataControl {
+  
+    private String host;
+    private int porta;
+    
+    public SendDataControl (String host, int porta) {
+        this.host = host;
+        this.porta = porta;
+    }
+
+	public void executa() throws UnknownHostException, IOException {
+        Socket cliente = new Socket(this.host, this.porta);
+        System.out.println("O cliente se conectou ao servidor!");
+
+        // thread para receber mensagens do servidor
+        Recebedor r = new Recebedor(cliente.getInputStream());
+        new Thread(r).start();
+
+        // lê msgs do teclado e manda pro servidor
+        Scanner teclado = new Scanner(System.in);
+        PrintStream saida = new PrintStream(cliente.getOutputStream());
+        while (teclado.hasNextLine()) {
+            saida.println(teclado.nextLine());
+        }
+
+        saida.close();
+        teclado.close();
+        cliente.close();        
+    }                
+    
+    public class Recebedor implements Runnable {
+
+        private InputStream servidor;
+
+        public Recebedor(InputStream servidor) {
+            this.servidor = servidor;
+        }
+
+        public void run() {
+            // recebe msgs do servidor e imprime na tela
+            Scanner s = new Scanner(this.servidor);
+            while (s.hasNextLine()) {
+                System.out.println(s.nextLine());
+            }
+        }
+    }
+  
+}
